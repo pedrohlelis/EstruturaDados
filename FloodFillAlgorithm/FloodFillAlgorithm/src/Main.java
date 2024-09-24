97f23ba2e3507aa289933631b57f081ce1b6159b
Gabriela Vieira Ramos <gabrielavieirar21@gmail.com>
2024-09-24 10:13:01 -0300

    Update Main.java

--- FloodFillAlgorithm/FloodFillAlgorithm/src/Main.java
+++ FloodFillAlgorithm/FloodFillAlgorithm/src/Main.java
@@ -9,14 +9,6 @@ import javax.imageio.ImageIO;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// 1. criar matriz 2 dimensoes com os pixels da imagem
-		// 2. solicitar uma coordenada de inicio do algoritmo e uma cor nova
-		// 3. validar coordenada e cor
-		// 4. run the conditions for painting it and enqueue the four pixels around it
-		// 5. dequeue a pixel, run the conditions for coloring it, and enqueue the four pixels around it
-		// 6. repeat until the queue becomes empty
-
 		
 		BufferedImage image;
 		try {
