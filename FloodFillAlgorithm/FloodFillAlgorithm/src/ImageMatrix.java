316ae5e52ad2714947615b807daf513bb0d929a9
Gabriela Vieira Ramos <gabrielavieirar21@gmail.com>
2024-09-24 10:12:32 -0300

    Update ImageMatrix.java

--- FloodFillAlgorithm/FloodFillAlgorithm/src/ImageMatrix.java
+++ FloodFillAlgorithm/FloodFillAlgorithm/src/ImageMatrix.java
@@ -25,10 +25,10 @@ public class ImageMatrix {
         	DoublyLinkedList newList = new DoublyLinkedList<Pixel>();
         	pixelMatrix.add(newList);
             for (int x = 0; x < width; x++) {
-                // Obtém a cor do pixel (x, y)
+
                 Color pixelColor = new Color(Image.getRGB(x, y));
 
-                // Cria um novo Pixel com suas coordenadas e cor
+
                 Pixel pixel = new Pixel(x, y, pixelColor);
 
                 newList.add(pixel);
@@ -37,9 +37,7 @@ public class ImageMatrix {
 	}
 	
 	public Pixel findPixel(int x, int y) throws Exception {
-//		if(x >= Length || y >= Height || x < 0 || y < 0) {
-//			return null;
-//		}
+
 		
 		DoublyLinkedList<Pixel> Yrow = (DoublyLinkedList<Pixel>) pixelMatrix.get(y);
 		
@@ -48,29 +46,28 @@ public class ImageMatrix {
     }
 	
 	public void printMatrix() throws Exception {
-	    // Assuming pixelMatrix is a list of rows, where each row is a DoublyLinkedList<Pixel>
+
 	    for (int i = 0; i < pixelMatrix.size(); i++) {
-	        // Get the row at index i
+
 	        DoublyLinkedList<Pixel> row = (DoublyLinkedList<Pixel>) pixelMatrix.get(i);
 	        
-	        // Print each pixel in the row
+
 	        for (int j = 0; j < row.size(); j++) {
-	            // Get the pixel at index j
+
 	            Pixel pixel = row.get(j);
 	            
-	            // Print the pixel's color or other representation
-	            if(pixel.color.getRed() == 255 && pixel.color.getGreen() == 255 && pixel.color.getBlue() == 255) {
-	            	System.out.print(1 + " "); // Or use another format for color
+				if(pixel.color.getRed() == 255 && pixel.color.getGreen() == 255 && pixel.color.getBlue() == 255) {
+	            	System.out.print(1 + " ");
 	            }
 	            else if(pixel.color.getRed() == 255 && pixel.color.getGreen() == 0 && pixel.color.getBlue() == 255) {
-	            	System.out.print(2 + " "); // Or use another format for color
+	            	System.out.print(2 + " ");
 	            }
 	            else {
 	            	System.out.print(0 + " ");
 	            }
 	        }
 	        
-	        // Move to the next line after each row
+
 	        System.out.println();
 	    }
 	}	
