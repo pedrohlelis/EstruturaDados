d1ec1018034a0ec5b8afc6702c1781658f34b814
Gabriela Vieira Ramos <gabrielavieirar21@gmail.com>
2024-09-24 10:14:07 -0300

    Update Queue.java

--- FloodFillAlgorithm/FloodFillAlgorithm/src/Queue.java
+++ FloodFillAlgorithm/FloodFillAlgorithm/src/Queue.java
@@ -7,10 +7,10 @@ public class Queue<T> {
 	private DoublyLinkedList<T> queue;
 	
 	public Queue() {
+
         queue = new DoublyLinkedList<>();
     }
 
-    // Add an element to the queue
     public void enqueue(T value) {
     	if(value == null) {
     		return;
@@ -18,7 +18,6 @@ public class Queue<T> {
         queue.add(value);
     }
 
-    // Remove an element from the queue
     public T dequeue() throws Exception {
         if (isEmpty()) {
             throw new Exception("Queue is empty");
@@ -26,12 +25,13 @@ public class Queue<T> {
         return queue.pop();
     }
 
-    // Check if the queue is empty
     public boolean isEmpty() {
         return queue.size() == 0;
     }
     
     public void printQueue() {
-    	queue.printList();
+
+        queue.printList();
+
     }
 }
