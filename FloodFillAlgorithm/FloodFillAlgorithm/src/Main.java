e00b90af9cf8a39d6dc8a996a3efcf196d5e2eea
Gabriela Vieira Ramos <gabrielavieirar21@gmail.com>
2024-09-24 10:21:55 -0300

    Update Main.java

--- FloodFillAlgorithm/FloodFillAlgorithm/src/Main.java
+++ FloodFillAlgorithm/FloodFillAlgorithm/src/Main.java
@@ -2,17 +2,16 @@ import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.LinkedList;
+
 
 import javax.imageio.ImageIO;
 
 public class Main {
-
 	public static void main(String[] args) {
 		
 		BufferedImage image;
 		try {
-			image = ImageIO.read(new File("D:/Users/Pedro/Pictures/flood_fill_input10x8.png"));
+			image = ImageIO.read(new File("FloodFillAlgorithm/FloodFillAlgorithm/src/tde.png"));
 			FloodFill flood = new FloodFill(image);
 			
 			Color newColor = new Color(255, 0, 255);
